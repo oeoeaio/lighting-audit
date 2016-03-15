a6038b9e5c856f6c373c0628fb3dd711d1593354
Rob Harrington <oeoeaio@gmail.com>
2016-03-15 19:07:25 +1100

    Adding user model for authentication

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml'
 gem 'foundation-rails'
 gem 'roo', '~> 2.1.0'
+gem 'devise'
+gem 'pundit'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215073602) do
+ActiveRecord::Schema.define(version: 20160315074834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,23 @@ ActiveRecord::Schema.define(version: 20160215073602) do
   add_index "switches", ["house_id"], name: "index_switches_on_house_id", using: :btree
   add_index "switches", ["number"], name: "index_switches_on_number", using: :btree
   add_index "switches", ["room_id"], name: "index_switches_on_room_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.integer  "sign_in_count",      default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.integer  "failed_attempts",    default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "lights", "houses"
   add_foreign_key "lights", "rooms"
